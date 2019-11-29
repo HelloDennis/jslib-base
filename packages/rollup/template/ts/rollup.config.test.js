a3cd14cd422963c0bd5a66a215812b05cb8aa207
HelloDennis <wumingliang@100tal.com>
2019-11-29 22:15:45 +0800

    feat: 添加在浏览器中进行测试的模式，以支持需要浏览器环境的模块的测试

---
+++ packages/rollup/template/ts/rollup.config.test.js
@@ -0,0 +1,30 @@
+// rollup.config.test.js
+// commonjs
+const serve = require('rollup-plugin-serve');
+const livereload = require('rollup-plugin-livereload');
+const common = require('./rollup.js');
+
+module.exports = {
+    input: 'test/index.ts',
+    output: {
+        file: 'test/index.js',
+        format: 'cjs',
+        // When export and export default are not used at the same time, set legacy to true.
+        // legacy: true,
+        // banner: common.banner,
+    },
+    plugins: [
+        common.getCompiler({
+            tsconfigOverride: { compilerOptions : { declaration: true, module: 'ES2015' } },
+            useTsconfigDeclarationDir: true
+        }),
+        serve({
+            contentBase: './',   //启动文件夹;
+            host: 'localhost',      //设置服务器;
+            port: 9999             //端口号;
+        }), 
+        livereload({
+            watch: './'     //监听文件夹;
+        })
+    ]
+};
